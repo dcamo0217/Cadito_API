7393860b3c67cae3821eee8ddd7da210f17c72e1
Diego Cavadia <89433295+dcamo0217@users.noreply.github.com>
2022-05-02 22:55:51 -0500

    feat: fetch post and recent post added

--- src/routes/post.js
+++ src/routes/post.js
@@ -3,17 +3,6 @@ const postSchema = require("../models/post");
 
 const router = express.Router();
 
-//FETCH USER POSTS
-router.get("/", async (req, res) => {
-  const { user_id } = req.query;
-  try {
-    const posts = await postSchema.find({ user_id });
-    return res.status(200).json(posts);
-  } catch (error) {
-    return res.status(500).json({ message: error });
-  }
-});
-
 //CREATE POST
 router.post("/", async (req, res) => {
   const { owner_id, img_url, display_name, description, price } = req.body;
@@ -26,6 +15,42 @@ router.post("/", async (req, res) => {
       price,
     });
     return res.status(201).json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
+//FETCH USER POSTS
+router.get("/", async (req, res) => {
+  const { user_id } = req.query;
+  try {
+    const posts = await postSchema.find({ user_id });
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
+//FETCH POST
+router.get("/", async (req, res) => {
+  const { post_id } = req.query;
+  try {
+    const post = await postSchema.findById(post_id);
+    if (post) {
+      return res.status(200).json(post);
+    } else {
+      return res.status(404).json({ message: "Post not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
+//FETCH RECENT POSTS
+router.get("/recent", async (req, res) => {
+  try {
+    const posts = await postSchema.find().sort({ created_date: -1 }).limit(5);
+    return res.status(200).json(posts);
   } catch (error) {
     return res.status(500).json({ message: error });
   }
